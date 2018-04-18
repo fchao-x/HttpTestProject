7f21c0dcbb24e103ee4ea7cd935d22a1c25b5d46
fchao.x <913298258@qq.com>
2018-04-18 20:38:32 +0800

    a
    a

--- HttpTest/views.py
+++ HttpTest/views.py
@@ -28,7 +28,7 @@ def index(request):
 	return render(request, 'base.html', {'item_list': item_list})
 
 def urlTest(request):
-	return render(request, '200 OK')
+	return render(request, 'URL.html', '200 OK')
 
 @csrf_exempt #禁用CSRF机制
 def postTest(request):
