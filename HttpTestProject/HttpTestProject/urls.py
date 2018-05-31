50b7af75ccbbd8d83c8d35259e8464647d005772
fchao-x <913298258@qq.com>
2018-05-31 01:53:40 -0400

    add HttpTestProject

---
+++ HttpTestProject/HttpTestProject/urls.py
@@ -0,0 +1,26 @@
+"""HttpTestProject URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import re_path, path
+from HttpTest import views as HttpTest_views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+
+    re_path(r'^index.html$', HttpTest_views.index, name='index'),
+    path('postTest', HttpTest_views.post_test, name='post_test'),
+    re_path(r'^URL.html$', HttpTest_views.url_test, name='url_test'),
+]
